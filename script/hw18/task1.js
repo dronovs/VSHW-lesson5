d23e6d447f93df69c42bb03545d5078a0b8e2f2f
dronovs <od270891dss@gmail.com>
2022-03-30 17:49:45 +0300

    update

--- script/hw18/task1.js
+++ script/hw18/task1.js
@@ -1,6 +1,29 @@
 'use strict'
-function padString (str, strLength, sym = ' ', pos = true) {
-    if (pos) {
-        
+function padString (str, stringLength, sym = ' ', pos = true) {
+    let i = str.length;
+    let result = '';
+
+    if (pos === true) {
+        if (stringLength <= i) {
+            result = str.substring(0, stringLength);
+        } else {
+            for (; i < stringLength; i++) {
+                result = str += sym;
+                console.log(result);
+            }
+        }
+    } else if (pos === false) {
+        if (stringLength <= i) {
+            result = str.substring(0, stringLength);
+        } else {
+            let strArr = str.split();
+            for (; i < stringLength; i++) {
+                strArr.unshift(sym);
+            }
+            result = strArr.join('');
+        }
     }
+    return result;
 }
+
+console.log(padString('0123456789', 15, '*',false));
