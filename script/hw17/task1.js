83fd584e265ae67784b86165964175929ee9b703
dronovs <od270891dss@gmail.com>
2022-04-04 12:59:11 +0300

    update

--- script/hw17/task1.js
+++ script/hw17/task1.js
@@ -1,8 +1,8 @@
 'use strict'
+let userNum = +prompt('Please enter a number!');
+let userDeg = +prompt('Please enter degree!');
 function numberDegree (num, deg) {
-     num = +prompt('Please enter a number!');
-     deg = +prompt('Please enter degree!');
      return Math.pow(num, deg);
 }
 
-console.log(numberDegree());
+console.log(numberDegree(userNum, userDeg));
