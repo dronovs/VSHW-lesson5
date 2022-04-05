5b6ac9b119a929023c805602d6de2b83be1f823f
dronovs <od270891dss@gmail.com>
2022-04-05 10:04:44 +0300

    update

--- script/hw20/task1.js
+++ script/hw20/task1.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const potatoWeight = 75;
+let potatoesPricePerKg = 13;
+
+function getPotatoesPrice (borschVolume) {
+    if (borschVolume < 1) return 'Invalid borsch volume';
+
+    const potatoesWeightPerLitter = (borschVolume * potatoWeight * 4) / 1000;
+    let totalPotatoesPrice = potatoesWeightPerLitter * potatoesPricePerKg;
+
+    return Math.round(totalPotatoesPrice);
+}
+
+console.log(getPotatoesPrice(2));
